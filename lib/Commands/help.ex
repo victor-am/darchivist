d1848ae40ea6220258514dfb3aec8e52e75393da
Victor Marques <victor.atmorning@gmail.com>
2019-01-05 03:47:08 -0200

    Add help command

---
+++ lib/Commands/help.ex
@@ -0,0 +1,39 @@
+defmodule Darchivist.Commands.Help do
+  alias Darchivist.Commands
+
+  @command_module_list [Commands.Help]
+
+  def syntax do
+    "help"
+  end
+
+  def description do
+    "Shows a description for each command"
+  end
+
+  def run(_params) do
+    header = [
+      "darchivist <INSERT VERSION HERE>",
+      "Usage: darchivist <command> [<args>]",
+      "",
+      "Available commands:"
+    ]
+
+    footer = [
+      "",
+      "For full documentation see <INSERT GITHUB LINK HERE>"
+    ]
+
+    header ++ commands_help() ++ footer
+  end
+
+  defp commands_help do
+    Enum.map(@command_module_list, fn command ->
+      help_for(command)
+    end)
+  end
+
+  defp help_for(command) do
+    "  #{command.syntax} - #{command.description}"
+  end
+end
---
+++ lib/cli.ex
@@ -0,0 +1,39 @@
+defmodule Darchivist.CLI do
+  alias Darchivist.Commands
+
+  @command_to_module_dictionary %{
+    "help" => Commands.Help
+  }
+
+  def main(args \\ []) do
+    args
+    |> parse_args
+    |> run_command
+    |> print_output
+  end
+
+  defp parse_args(args) do
+    args
+    |> OptionParser.parse(switches: [])
+    |> build_command_params
+  end
+
+  # Redirects to the `help` command when no command was given.
+  defp build_command_params({_options, [], _}) do
+    build_command_params({[], ["help"], []})
+  end
+
+  defp build_command_params({options, params, _}) do
+    [command_name | command_params] = params
+    [command: command_name, params: command_params, options: options]
+  end
+
+  defp run_command(command: command, params: params, options: _options) do
+    command = @command_to_module_dictionary[command]
+    command.run(params)
+  end
+
+  defp print_output(command_output_lines) do
+    Enum.each(command_output_lines, &IO.puts/1)
+  end
+end
