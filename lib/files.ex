c906657c1e8454350284ad376fa07c61849cc524
Victor Marques <victor.atmorning@gmail.com>
2019-01-07 16:16:13 -0200

    Add sample_manifest as an external resource

--- lib/files.ex
+++ lib/files.ex
@@ -1,9 +1,8 @@
 defmodule Darchivist.Files do
-  # This is being done in order to bundle the template files together
-  # with the darchivist binary.
-  @sample_manifest_file Path.relative_to_cwd("priv/sample_manifest.yaml") |> File.read!()
+  @sample_manifest_path Path.relative_to_cwd("priv/sample_manifest.yaml")
+  @sample_manifest_content File.read!(@sample_manifest_path)
 
-  def sample_manifest do
-    @sample_manifest_file
-  end
+  @external_resource @sample_manifest_path
+
+  def sample_manifest, do: @sample_manifest_content
 end
